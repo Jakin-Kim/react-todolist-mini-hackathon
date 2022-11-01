015c65fdbb69dbd7d3bc07af71ea610c94c91238
Jakin-Kim <kimyagin94@gmail.com>
2022-11-02 01:47:00 +0900

    feat/App: 컴포넌트 최상단에 TodoProvider로 감싸기

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoHead from './components/TodoHead'; // 2.
 import TodoItemList from "./components/TodoItemList"; // 3.
 // import TodoItem from "./components/TodoItem"; // 3-1.
 import TodoCreate from './components/TodoCreate'; // 4.
+import { TodoProvider } from "./TodoContext";
 
 // 화면 전체에 회색 배경색 적용하기(todo list와 구분하기 위해)
 const GlobalStyle = createGlobalStyle`
@@ -18,12 +19,14 @@ function App() {
   
   return (
     <>
+    <TodoProvider>
       <GlobalStyle/>
       <TodoTemplate>
         <TodoHead /> 
         <TodoItemList/>
         <TodoCreate/>
       </TodoTemplate>
+    </TodoProvider>
     </>
   );
 }
--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,6 +1,38 @@
 // 오늘의 날짜와 요일을 보여주고 앞으로 해야할 일이 몇개 남았는지 보여주는 컴포넌트
 import React from 'react';
 import styled from 'styled-components';
+import { useTodoState } from '../TodoContext';
+
+// 오늘의 날짜, 요일, 남은 할 일의 갯수를 보여주는 TodoHead 컴포넌트
+function TodoHead() {
+
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
+  const today = () => {
+    if(now.getDay() === 0) return '일요일'
+    if(now.getDay() === 1) return '월요일'
+    if(now.getDay() === 2) return '화요일'
+    if(now.getDay() === 3) return '수요일'
+    if(now.getDay() === 4) return '목요일'
+    if(now.getDay() === 5) return '금요일'
+    if(now.getDay() === 6) return '토요일'
+  }
+
+  const todos = useTodoState();
+  console.log(todos);
+  
+  return (
+    <TodoHeadBlock>
+      <h1>{year}년 {month}월 {date}일</h1>
+      <div className="day">{today()}</div>
+      <div className="tasks-left">할 일 2개 남음</div>
+    </TodoHeadBlock>
+  );
+}
+
+export default TodoHead;
 
 const TodoHeadBlock = styled.div`
   padding: 48px 32px 24px 32px; // 요소가 4개라면, top | right | bottom | left (시계방향으로 회전한다고 생각하면 쉽겠다!)
@@ -22,30 +54,3 @@ const TodoHeadBlock = styled.div`
     font-weight: bold;
   }
 `;
-
-// 오늘의 날짜, 요일, 남은 할 일의 갯수를 보여주는 TodoHead 컴포넌트
-function TodoHead() {
-
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = now.getMonth() + 1;
-  const date = now.getDate();
-  const today = () => {
-    if(now.getDay() === 0) return '일요일'
-    if(now.getDay() === 1) return '월요일'
-    if(now.getDay() === 2) return '화요일'
-    if(now.getDay() === 3) return '수요일'
-    if(now.getDay() === 4) return '목요일'
-    if(now.getDay() === 5) return '금요일'
-    if(now.getDay() === 6) return '토요일'
-  }
-  
-  return (
-    <TodoHeadBlock>
-      <h1>{year}년 {month}월 {date}일</h1>
-      <div className="day">{today()}</div>
-      <div className="tasks-left">할 일 2개 남음</div>
-    </TodoHeadBlock>
-  );
-}
-export default TodoHead;
