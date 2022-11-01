4f30a8ff25979a786f180ba481d48ca7a279c45b
Jakin-Kim <kimyagin94@gmail.com>
2022-11-02 02:55:29 +0900

    feat/TodoItemList: 할 일 목록 데이터를 실시간으로 반영되도록 변경

--- src/components/TodoItemList.js
+++ src/components/TodoItemList.js
@@ -1,15 +1,26 @@
-import React, {  } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import TodoItem from './TodoItem';
+import { useTodoState } from '../TodoContext';
 
 // 여러개의 할 일 리스트를 보여줄 TodoList 컴포넌트
 function TodoItemList() {
+  const todos = useTodoState();
+  
   return (
     <TodoListBlock>
-      <TodoItem text="프로젝트 생성하기" done={true} />
+      {todos.map(todo => 
+        <TodoItem 
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      )}
+      {/* <TodoItem text="프로젝트 생성하기" done={true} />
       <TodoItem text="컴포넌트 스타일링 하기" done={true} />
       <TodoItem text="Context 만들기" done={false} />
-      <TodoItem text="기능 구현하기" done={false} />
+      <TodoItem text="기능 구현하기" done={false} /> */}
     </TodoListBlock>
   );
 }
