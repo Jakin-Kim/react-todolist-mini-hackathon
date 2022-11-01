f4d574b60fecff9faec87380869370a2d1e0df81
Jakin-Kim <kimyagin94@gmail.com>
2022-11-01 23:12:37 +0900

    feat/TodoContext: TodoContext파일 안에 useReducer를 사용하여 상태를 관리하는 TodoProvider 컴포넌트 생성완료

--- src/TodoContext.js
+++ src/TodoContext.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 
-function TodoContext() {
-  return (
-    <div>TodoContext</div>
-  )
+const initialTodos = [
+  {
+    id: 1,
+    text: '프로젝트 생성하기',
+    done: true,
+  },
+  {
+    id: 2,
+    text: '컴포넌트 스타일링하기',
+    done: true,
+  },
+  {
+    id: 3,
+    text: 'Context 만들기',
+    done: false,
+  },
+  {
+    id: 4,
+    text: '기능 구현하기',
+    done: false,
+  },
+];
+
+const CREATE = 'CREATE';
+const TOGGLE = 'TOGGLE';
+const REMOVE = 'REMOVE';
+
+function todoReducer(state, action) {
+  switch(action.type) {
+    case CREATE:
+      return state.concat(action.todo);
+    case TOGGLE:
+      return state.map(todo => 
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case REMOVE:
+      return state.filter(todo => todo.id !== action.id);
+
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
 }
 
-export default TodoContext
+export function TodoProvider({ children }) {
+  const [ state, dispatch ] = useReducer(todoReducer, initialTodos);
+  return children;
+}
